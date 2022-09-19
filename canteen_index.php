62fd5285b1a45bd094f11370b6ef90613c0687bb
crpage06 <110359460+crpage06@users.noreply.github.com>
2022-09-19 12:38:01 +1200

    Connected index page to the databse

---
+++ canteen_index.php
@@ -0,0 +1,11 @@
+<?php
+/* Connect to the database */
+$dbcon = mysqli_connect("localhost", "carinapage", "gLs5CJg", "carinapage_canteen");
+
+if($dbcon == NULL) {
+    echo "Could not connect to database";
+    exit(); }
+else {
+	echo "Successfully Connected to database";
+}
+?>
