27dc0b9ab5d58328b6fed376f930d54bcaa25711
crpage06 <110359460+crpage06@users.noreply.github.com>
2022-09-19 12:39:56 +1200

    Created basic table for first specials item

--- canteen_specials.php
+++ canteen_specials.php
@@ -24,3 +24,48 @@ else {
     /* Fetch the result into an associative array */
     $this_specials_record = mysqli_fetch_assoc($this_specials_result)	
 ?>
+
+<!DOCTYPE html>	
+<html lang="en">
+	
+	<head>
+        	<meta charset="utf-8">
+        	<title></title>
+
+        	<meta name="description" content="">
+        	<link href="css/styles.css" rel="stylesheet">
+
+	</head>
+	<style>
+	table, th, td {
+  	border:1px solid black;
+		border-collapse: collapse
+	}
+	th, td{
+		background-color: white
+		}
+	</style>
+	<body>
+		<h1>Canteen Menu</h1>
+		<h2>Weekly Specials</h2>
+
+		<table style="width:50%">
+		<tr>
+			<th>Special</th>
+			<th>Cost</th>
+			<th>Status</th>
+		</tr>
+		<tr>
+			<td><?php
+				echo "" .$this_specials_record['weekly_special']. "";
+			?> </td>
+			<td><?php
+				echo "$" .$this_specials_record['cost']. "";
+			?> </td>
+			<td><?php
+				echo "" .$this_specials_record['status']. "";
+			?> </td>
+		</tr>
+		</table>
+	</body>
+</html>
