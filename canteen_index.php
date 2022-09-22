b2805d97054422dad910a780334b3a4fbee53566
crpage06 <110359460+crpage06@users.noreply.github.com>
2022-09-23 09:10:44 +1200

    Created the Home menu
    Added buttons to travel to the "Drinks and Food" and "Specials" menus as well as a message saying what this week's special is.

--- canteen_index.php
+++ canteen_index.php
@@ -5,7 +5,30 @@ $dbcon = mysqli_connect("localhost", "carinapage", "gLs5CJg", "carinapage_cantee
 if($dbcon == NULL) {
     echo "Could not connect to database";
     exit(); }
-else {
-	echo "Successfully Connected to database";
-}
+
+/* Create a query that grabs only the specials menu items that are in stock */
+    $this_specials_query = "SELECT * FROM specials WHERE specials.status = 'in stock' ";
+
+    /* Perform the query against the database */
+    $this_specials_result = mysqli_query($dbcon, $this_specials_query);
+
+    /* Fetch the result into an associative array */
+    $this_specials_record = mysqli_fetch_assoc($this_specials_result);
+
 ?>
+<!DOCTYPE html>
+<html>
+	<body>
+		<h1>WEGC Canteen Menu</h1>
+		<p>Welcome to the Wellington East Girls' College Canteen website. Here you can find the menu for our school canteen to see what we have to offer. Click the "Drinks and Food" Button to look at the drinks and food available, or click the "Weekly Specials" button to look at what our specials are.</p>
+		
+		<button onclick="document.location='canteen_drinks.php'">Drinks and Food</button>
+		<button onclick="document.location='canteen_specials.php'">Weekly Specials</button>
+		<br>
+		
+		<?php
+		echo "This weeks special is " . $this_specials_record['weekly_special'] . "";
+		?>
+		 
+	</body>
+</html>
